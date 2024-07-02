58b48fc65b0480e180ebdbeee33c704daec2530e
Denys Bokov <denisbokov1703@gmail.com>
2024-07-02 20:47:37 +0200

    feat(App): create first simple request using tan stack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import './App.module.scss';
+import { spawn } from 'child_process';
+
+const BASE_URL="https://jsonplaceholder.typicode.com";
 
 function App() {
+  const { isLoading, data } = useQuery({
+    queryKey: ['coins'],
+    queryFn: () => fetch(`${BASE_URL}/todos`).then(response => response.json()),
+  });
+
+  if (isLoading) return <span>loading...</span>;
+
   return (
     <section>
-      <h1>TSQAPP</h1>
+      <h1>Tan Stack Query</h1>
+
+      <ul>
+        {data.map((item: ITodo) => (
+          <li key={item.id}>
+            <span>{item.title}:</span>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
