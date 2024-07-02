0761a07380dbace8f96affca65f5fe14b1ca5ea8
Denys Bokov <denisbokov1703@gmail.com>
2024-07-02 20:46:41 +0200

    feat(App): query client init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { container } = render(<App />);
+  const { container } = render(
+    <App />,
+    { wrapper: () => <QueryClientProvider client={new QueryClient()} />}
+  );
   expect(container).toMatchSnapshot();
 });
