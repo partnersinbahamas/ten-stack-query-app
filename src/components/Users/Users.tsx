e6d8e101e0aba63bc551c5664c52e8abfd8fa1ef
Denys Bokov <denisbokov1703@gmail.com>
2024-07-05 15:32:57 +0200

    stytes(Users): add styles

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,15 +1,16 @@
-import User from "./User/User";
+import styles from './Users.module.scss';
 
 type TProps = {
   users: IUser[];
+  onUserSelect: (id: TID) => void,
 };
 
-export const Users: React.FC<TProps> = ({ users }) => (
-  <ul>
+export const Users: React.FC<TProps> = ({ users, onUserSelect }) => (
+  <ul className={styles.users}>
     {users.map((user: IUser) => (
-    <li key={user.id}>
-        <User user={user} />
-    </li>
+      <li key={user.id} className={styles.user} onClick={() => onUserSelect(user.id)}>
+        <span>{user.name}</span>
+      </li>
     ))}
   </ul>
 );
