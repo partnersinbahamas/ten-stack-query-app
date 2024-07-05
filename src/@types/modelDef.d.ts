c5b2af76509c11223582858b2c18c985e1d5d33e
Denys Bokov <denisbokov1703@gmail.com>
2024-07-05 14:13:50 +0200

    types(modelDef.d.ts): create type for User

--- src/@types/modelDef.d.ts
+++ src/@types/modelDef.d.ts
@@ -14,3 +14,35 @@ interface ITodo {
   completed: boolean;
   title: string
 };
+
+interface IUserGeo {
+  lat: string;
+  lng: string;
+}
+
+interface IUserAdress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: IUserGeo;
+};
+
+interface IUserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface IUser {
+  id: TID;
+  name: string;
+  username: string;
+  email: string;
+  address: IUserAdress;
+  phone: string;
+  website: string;
+  company: IUserCompany;
+}
+  
+  
